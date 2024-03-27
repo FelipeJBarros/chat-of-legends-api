72528a492a537c5e708415188f3e7de47fd0d7bf
Felipe Barros <felipebarros.engh@gmail.com>
2024-03-27 14:47:06 -0300

    feat: initial commit

---
+++ src/main/java/com/flpbrrs/lca/Application.java
@@ -0,0 +1,13 @@
+package com.flpbrrs.lca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/flpbrrs/lca/ChatLeagueOfLegendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flpbrrs.lca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatLeagueOfLegendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
