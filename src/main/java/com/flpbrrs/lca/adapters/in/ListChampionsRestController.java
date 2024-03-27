6df6d48d97ed269aade20e63c440b43987ab66a8
Felipe Barros <felipebarros.engh@gmail.com>
2024-03-27 15:42:10 -0300

    docs: adding openapi tags for list champions endpoints

--- src/main/java/com/flpbrrs/lca/adapters/in/ListChampionsRestController.java
+++ src/main/java/com/flpbrrs/lca/adapters/in/ListChampionsRestController.java
@@ -2,12 +2,14 @@ package com.flpbrrs.lca.adapters.in;
 
 import com.flpbrrs.lca.application.ListChampionUseCase;
 import com.flpbrrs.lca.domain.models.Champion;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name="Champions", description = "Endpoints of league of legends champions domain")
 @RestController
 @RequestMapping("/champions")
 public record ListChampionsRestController(ListChampionUseCase useCase) {
