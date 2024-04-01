96996361aea3131da1249aa546d3838b2f639d13
Felipe Barros <felipebarros.engh@gmail.com>
2024-04-01 11:15:39 -0300

    feat: adding list by id endpoint

--- src/main/java/com/flpbrrs/lca/adapters/in/ListChampionsRestController.java
+++ src/main/java/com/flpbrrs/lca/adapters/in/ListChampionsRestController.java
@@ -4,6 +4,7 @@ import com.flpbrrs.lca.application.ListChampionUseCase;
 import com.flpbrrs.lca.domain.models.Champion;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public record ListChampionsRestController(ListChampionUseCase useCase) {
     @GetMapping
     public List<Champion> findAllChampions() {
         return useCase.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Champion findByChampionId(@PathVariable Long id) {
+        return useCase.findById(id);
     }
 }
--- src/main/java/com/flpbrrs/lca/application/ListChampionUseCase.java
+++ src/main/java/com/flpbrrs/lca/application/ListChampionUseCase.java
@@ -1,5 +1,6 @@
 package com.flpbrrs.lca.application;
 
+import com.flpbrrs.lca.domain.exceptions.ChampionNotFoundException;
 import com.flpbrrs.lca.domain.models.Champion;
 import com.flpbrrs.lca.domain.ports.ChampionsRepository;
 
@@ -8,5 +9,9 @@ import java.util.List;
 public record ListChampionUseCase(ChampionsRepository repository) {
     public List<Champion> findAll() {
         return repository.findAll();
+    }
+
+    public Champion findById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new ChampionNotFoundException(id));
     }
 }
